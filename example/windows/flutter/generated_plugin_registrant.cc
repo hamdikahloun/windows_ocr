abfcf72709b1db026e61de5ee8b3df336742e890
Hamdi Kahloun <hamdikahlounacil@gmail.com>
2020-12-28 10:40:14 +0100

    first commit

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <windows_ocr/windows_ocr_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowsOcrPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowsOcrPlugin"));
+}
---
+++ test/windows_ocr_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:windows_ocr/windows_ocr.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('windows_ocr');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await WindowsOcr.platformVersion, '42');
+  });
+}
